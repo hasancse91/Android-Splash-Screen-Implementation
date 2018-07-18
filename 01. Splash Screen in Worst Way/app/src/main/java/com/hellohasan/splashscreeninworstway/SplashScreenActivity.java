c404e8d6bcd8f8913d9e0e6ad7fe009ab3e8ab0e
Abdullah Al Hasan <hasan.cse91@gmail.com>
2018-07-18 18:41:58 +0600

    wosrt implementation of Splash screen project done

---
+++ 01. Splash Screen in Worst Way/app/src/main/java/com/hellohasan/splashscreeninworstway/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.hellohasan.splashscreeninworstway;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+}
