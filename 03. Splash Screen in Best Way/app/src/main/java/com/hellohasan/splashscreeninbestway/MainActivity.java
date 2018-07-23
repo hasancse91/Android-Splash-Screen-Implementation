ca9925c3d1ecf2cf9fcc5edc20a89836d82398eb
Abdullah Al Hasan <hasan.cse91@gmail.com>
2018-07-23 18:27:42 +0600

    best implementation done

--- 03. Splash Screen in Best Way/app/src/main/java/com/hellohasan/splashscreeninbestway/MainActivity.java
+++ 03. Splash Screen in Best Way/app/src/main/java/com/hellohasan/splashscreeninbestway/MainActivity.java
@@ -7,7 +7,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Set your theme before super.onCreate() to replace your previous theme of Manifest
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: add your MainActivity code here
     }
 }
