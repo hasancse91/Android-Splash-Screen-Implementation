1cb170f6f304dc55ccd8dc245d807f6d610129fc
Hasan Abdullah <hasan.cse91@gmail.com>
2019-10-31 10:42:51 +0600

    migrate to androidx, 1st project

--- 01. Splash Screen in Worst Way/app/src/main/java/com/hellohasan/splashscreeninworstway/SplashScreenActivity.java
+++ 01. Splash Screen in Worst Way/app/src/main/java/com/hellohasan/splashscreeninworstway/SplashScreenActivity.java
@@ -2,7 +2,7 @@ package com.hellohasan.splashscreeninworstway;
 
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
